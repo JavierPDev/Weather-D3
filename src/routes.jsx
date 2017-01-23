c1d52735596a8702d33e29e603a0936b258efefe
Javier Pena <javier@javierdev.com>
2017-01-23 12:40:57 -0600

    Remove about component

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,14 +3,12 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Main from 'Main';
 import Forecast from 'Forecast';
-import About from 'About';
 
 export const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Forecast} />
       <Route path="forecast" component={Forecast} />
-      <Route path="about" component={About} />
     </Route>
   </Router>
 )
